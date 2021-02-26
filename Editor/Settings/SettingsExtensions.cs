c6359a6568939e4ec6e1441a507ff111bc3a1f13
Douglas Cox <51918435+Hextant@users.noreply.github.com>
2021-02-26 17:26:38 -0500

    Added Settings class for custom runtime and editor settings.

---
+++ Editor/Settings/SettingsExtensions.cs
@@ -0,0 +1,18 @@
+// Copyright 2021 by Hextant Studios. https://HextantStudios.com
+// This work is licensed under CC BY 4.0. http://creativecommons.org/licenses/by/4.0/
+using UnityEditor;
+
+namespace Hextant.Editor
+{
+    public static class SettingsExtensions
+    {
+        // The SettingsProvider instance used to display settings in Edit/Preferences
+        // and Edit/Project Settings.
+        public static SettingsProvider GetSettingsProvider<T>(
+            this Settings<T> settings ) where T : Settings<T> =>
+            new ScriptableObjectSettingsProvider( settings,
+                Settings<T>.attribute.usage == SettingsUsage.EditorUser ?
+                SettingsScope.User : SettingsScope.Project,
+                Settings<T>.attribute.displayPath );
+    }
+}
